c082f11b75ad0f603ed5afb2d2bbeb7fdd74d5eb
Andrii Khalin <74684444+AndriiKhalin@users.noreply.github.com>
2023-10-22 20:37:09 +0300

    create Shops

---
+++ RentShop_API/Models/Entities/Shops/Shops.cs
@@ -0,0 +1,10 @@
+﻿namespace RentShop_API.Models.Entities.Shops;
+
+public class Shops
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Address { get; set; }
+}
