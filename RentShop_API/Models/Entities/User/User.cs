7b27833514fe50b84c010bece44e6be42570ec0e
Andrii Khalin <74684444+AndriiKhalin@users.noreply.github.com>
2023-10-22 20:20:27 +0300

    create Model User (#3)

--- RentShop_API/Models/Entities/User/User.cs
+++ RentShop_API/Models/Entities/User/User.cs
@@ -2,5 +2,21 @@
 {
     public class User
     {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Password { get; set; }
+
+        public string Status { get; set; }
+
     }
 }
