a7b9a8bfefa4c68d84cd66b9fc5f3dd11a69888f
Andrii Khalin <74684444+AndriiKhalin@users.noreply.github.com>
2023-10-23 15:11:27 +0300

    Andrew_AddEntities(Cat,Ord,User,Shop,Trans)

--- RentShop_API/Models/Entities/Order.cs
+++ RentShop_API/Models/Entities/Order.cs
@@ -2,5 +2,18 @@
 
 public class Order
 {
+    public Guid Id { get; set; }
 
+    public float Price { get; set; }
+
+    public DateTime DateFrom { get; set; }
+
+    public DateTime DateTo { get; set; }
+
+    public Guid UserId { get; set; }
+    public User? User { get; set; }
+
+    public List<Shop> Shops { get; set; } = new();
+
+    public List<Transport> Transports { get; set; } = new();
 }
--- RentShop_API/Models/Entities/Transport.cs
+++ RentShop_API/Models/Entities/Transport.cs
@@ -2,5 +2,23 @@
 
 public class Transport
 {
+    public Guid Id { get; set; }
 
+    public string Model { get; set; }
+
+    public string Mark { get; set; }
+
+    public float PriceMinute { get; set; }
+
+    public int MaxSpeed { get; set; }
+
+    public string ImgUrl { get; set; }
+
+    public int MaxWeight { get; set; }
+
+    public Guid CategoryId { get; set; }
+
+    public Category? Category { get; set; }
+
+    public List<Order> Orders { get; set; } = new();
 }
