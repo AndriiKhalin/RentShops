1697ebdbc40c8c1240e0efb2abc734098a8e3036
Andrii Khalin <74684444+AndriiKhalin@users.noreply.github.com>
2023-10-23 14:34:57 +0300

    add Order

--- RentShop_API/Models/Entities/Order.cs
+++ RentShop_API/Models/Entities/Order.cs
@@ -2,5 +2,14 @@
 
 public class Order
 {
+    public Guid Id { get; set; }
 
+    public float Price { get; set; }
+
+    public DateTime DateFrom { get; set; }
+
+    public DateTime DateTo { get; set; }
+
+    public Guid UserId { get; set; }
+    public User? User { get; set; }
 }
