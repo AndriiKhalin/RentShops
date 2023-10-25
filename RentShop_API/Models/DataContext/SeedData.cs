e5eb42a67f6e68ab194549ea2a65996cff9ece60
Andrii Khalin <74684444+AndriiKhalin@users.noreply.github.com>
2023-10-25 21:46:38 +0300

    add another data

--- RentShop_API/Models/DataContext/SeedData.cs
+++ RentShop_API/Models/DataContext/SeedData.cs
@@ -12,26 +12,200 @@ public static class SeedData
 {
     public static void SeedUsers(this ModelBuilder modelBuilder)
     {
-
+        modelBuilder.Entity<User>().HasData(
+            new User
+            {
+                Id = Guid.NewGuid(),
+                Name = "Andrew",
+                LastName = "Khalin",
+                Email = "khalin2002@gmail.com",
+                Password = "10122002",
+                Phone = "+380737303288",
+                BirthDate = new DateTime(2002, 12, 10),
+                Orders = new List<Order>() { },
+                Ratings = new List<Rating>(),
+                Role = "User"
+            },
+            new User
+            {
+                Id = Guid.NewGuid(),
+                Name = "Vanya",
+                LastName = "Libid",
+                Email = "libid@gmail.com",
+                Password = "1920202",
+                Phone = "+380737303277",
+                BirthDate = new DateTime(2001, 10, 5),
+                Orders = new List<Order>() { },
+                Ratings = new List<Rating>(),
+                Role = "User"
+            },
+            new User
+            {
+                Id = Guid.NewGuid(),
+                Name = "Vlad",
+                LastName = "Shabaltas",
+                Email = "shabaltas@gmail.com",
+                Password = "648577",
+                Phone = "+380991833277",
+                BirthDate = new DateTime(2004, 2, 18),
+                Orders = new List<Order>() { },
+                Ratings = new List<Rating>(),
+                Role = "Admin"
+            }
+        );
     }
 
     public static void SeedOrders(this ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Order>().HasData(
+            new Order
+            {
+                Id = Guid.NewGuid(),
+                DateFrom = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 12, 55, 03),
+                DateTo = DateTime.Now,
+                Price = 55f,
+                User = new User(),
+                Transaction = new Transaction(),
+                Shops = new List<Shop>(),
+                Transports = new List<Transport>(),
+                UserId = Guid.Empty
+
+            },
+            new Order
+            {
+                Id = Guid.NewGuid(),
+                DateFrom = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 12, 33, 13),
+                DateTo = DateTime.Now,
+                Price = 35f,
+                User = new User(),
+                Transaction = new Transaction(),
+                Shops = new List<Shop>(),
+                Transports = new List<Transport>(),
+                UserId = Guid.Empty
+
+            },
+            new Order
+            {
+                Id = Guid.NewGuid(),
+                DateFrom = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 11, 25, 53),
+                DateTo = DateTime.Now,
+                Price = 155f,
+                User = new User(),
+                Transaction = new Transaction(),
+                Shops = new List<Shop>(),
+                Transports = new List<Transport>(),
+                UserId = Guid.Empty
+
+            }
+        );
     }
 
     public static void SeedTransports(this ModelBuilder modelBuilder)
     {
-
+        modelBuilder.Entity<Transport>().HasData(
+            new Transport()
+            {
+                Id = Guid.NewGuid(),
+                Mark = "Honda",
+                Model = "V3",
+                PriceMinute = 3.5f,
+                ImgUrl = "http://...",
+                MaxSpeed = 35,
+                MaxWeight = 125,
+                Ratings = new List<Rating>(),
+                Category = new Category(),
+                Orders = new List<Order>(),
+                TransportAvailable = new TransportAvailable(),
+                CategoryId = Guid.Empty
+            },
+            new Transport()
+            {
+                Id = Guid.NewGuid(),
+                Mark = "Tesla",
+                Model = "Skod",
+                PriceMinute = 4.5f,
+                ImgUrl = "http://...",
+                MaxSpeed = 45,
+                MaxWeight = 115,
+                Ratings = new List<Rating>(),
+                Category = new Category(),
+                Orders = new List<Order>(),
+                TransportAvailable = new TransportAvailable(),
+                CategoryId = Guid.Empty
+            },
+            new Transport()
+            {
+                Id = Guid.NewGuid(),
+                Mark = "Volva",
+                Model = "Speed",
+                PriceMinute = 3f,
+                ImgUrl = "http://...",
+                MaxSpeed = 30,
+                MaxWeight = 105,
+                Ratings = new List<Rating>(),
+                Category = new Category(),
+                Orders = new List<Order>(),
+                TransportAvailable = new TransportAvailable(),
+                CategoryId = Guid.Empty
+            }
+        );
     }
 
     public static void SeedShops(this ModelBuilder modelBuilder)
     {
-
+        modelBuilder.Entity<Shop>().HasData(
+            new Shop()
+            {
+                Id = Guid.NewGuid(),
+                Address = "Street Victory 5",
+                TransportAvailable = new TransportAvailable(),
+                Orders = new List<Order>(),
+                WorkTimeStart = new TimeOnly(8, 0),
+                WorkTimeEnd = new TimeOnly(18, 0)
+            },
+            new Shop()
+            {
+                Id = Guid.NewGuid(),
+                Address = "Street Livikovicha 15",
+                TransportAvailable = new TransportAvailable(),
+                Orders = new List<Order>(),
+                WorkTimeStart = new TimeOnly(8, 0),
+                WorkTimeEnd = new TimeOnly(18, 0)
+            },
+            new Shop()
+            {
+                Id = Guid.NewGuid(),
+                Address = "Street Chresatic 55",
+                TransportAvailable = new TransportAvailable(),
+                Orders = new List<Order>(),
+                WorkTimeStart = new TimeOnly(8, 0),
+                WorkTimeEnd = new TimeOnly(18, 0)
+            }
+        );
     }
 
     public static void SeedCategories(this ModelBuilder modelBuilder)
     {
-
+        modelBuilder.Entity<Category>().HasData(
+            new Category()
+            {
+                Id = Guid.NewGuid(),
+                Name_Category = "Scooter",
+                Transports = new List<Transport>()
+            },
+            new Category()
+            {
+                Id = Guid.NewGuid(),
+                Name_Category = "Bike",
+                Transports = new List<Transport>()
+            },
+            new Category()
+            {
+                Id = Guid.NewGuid(),
+                Name_Category = "Motorbike",
+                Transports = new List<Transport>()
+            }
+        );
     }
 
     public static void SeedTransactions(this ModelBuilder modelBuilder)
@@ -40,7 +214,7 @@ public static class SeedData
             new Transaction
             {
                 Id = Guid.NewGuid(),
-                Sum = 0,
+                Sum = 125,
                 Date = DateTime.Now,
                 OrderId = Guid.Empty,
                 Order = new Order(),
@@ -66,11 +240,11 @@ public static class SeedData
         modelBuilder.Entity<TransportAvailable>().HasData(
             new TransportAvailable
             {
-                Id = Guid.NewGuid(),    
+                Id = Guid.NewGuid(),
                 CountTransport = 0,
                 TransportId = Guid.Empty,
                 Transport = new Transport(),
-                ShopId = Guid.Empty,  
+                ShopId = Guid.Empty,
                 Shop = new Shop(),
             }
             );
@@ -81,8 +255,8 @@ public static class SeedData
             new Rating
             {
                 Id = Guid.NewGuid(),
-                Grand = "",
-                Comment = "",
+                Grand = 2,
+                Comment = "Good",
                 CreatedAt = DateTime.UtcNow,
                 UserId = Guid.Empty,
                 User = new User(),
--- RentShop_API/Models/Entities/Rating.cs
+++ RentShop_API/Models/Entities/Rating.cs
@@ -3,13 +3,13 @@
 public class Rating
 {
     public Guid Id { get; set; }
-    public string Grand { get; set; }
-    public string Comment { get; set; } 
-    public DateTime CreatedAt{get; set; }
+    public int Grand { get; set; }
+    public string Comment { get; set; }
+    public DateTime CreatedAt { get; set; }
     public Guid UserId { get; set; }
-    public User? User { get; set; } 
+    public User? User { get; set; }
     public Guid TransportsId { get; set; }
-    public Transport? Transport { get; set; }   
-    
+    public Transport? Transport { get; set; }
+
 
 }
