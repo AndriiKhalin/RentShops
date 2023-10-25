7d1a23c633f6725d6197253bec20a03276aff687
Ivan Lebid <lebidivan707@gmail.com>
2023-10-25 20:51:25 +0300

    Seeded Entities Rating, TransportAvailable, LogTransaction, Transaction

--- RentShop_API/Models/DataContext/SeedData.cs
+++ RentShop_API/Models/DataContext/SeedData.cs
@@ -36,19 +36,59 @@ public static class SeedData
 
     public static void SeedTransactions(this ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Transaction>().HasData(
+            new Transaction
+            {
+                Id = Guid.NewGuid(),
+                Sum = 0,
+                Date = DateTime.Now,
+                OrderId = Guid.Empty,
+                Order = new Order(),
+                LogTransaction = new LogTransaction()
+            });
     }
 
     public static void SeedLogTransactions(this ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<LogTransaction>().HasData(
+            new LogTransaction
+            {
+                Id = Guid.NewGuid(),
+                Results = true,
+                TransactionId = Guid.Empty,
+                Transaction = new Transaction()
+            });
 
     }
 
     public static void SeedTransportAvailable(this ModelBuilder modelBuilder)
     {
-
+        modelBuilder.Entity<TransportAvailable>().HasData(
+            new TransportAvailable
+            {
+                Id = Guid.NewGuid(),    
+                CountTransport = 0,
+                TransportId = Guid.Empty,
+                Transport = new Transport(),
+                ShopId = Guid.Empty,  
+                Shop = new Shop(),
+            }
+            );
     }
     public static void SeedRating(this ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Rating>().HasData(
+            new Rating
+            {
+                Id = Guid.NewGuid(),
+                Grand = "",
+                Comment = "",
+                CreatedAt = DateTime.UtcNow,
+                UserId = Guid.Empty,
+                User = new User(),
+                TransportsId = Guid.Empty,
+                Transport = new Transport()
 
+            });
     }
 }
