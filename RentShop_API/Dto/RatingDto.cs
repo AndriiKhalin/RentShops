efebd0d6864dfa795e8536aeed1115db86eb9dee
Андрій Халін <andrii.khalin@nure.ua>
2024-02-02 18:40:37 +0200

    create api

--- RentShop_API/Dto/RatingDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RentShop_API.Dto;
-
-public class RatingDto
-{
-    public Guid Id { get; set; }
-    public int Grand { get; set; }
-    public string? Comment { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
--- RentShop_API/globalUsing.cs
+++
@@ -1,7 +0,0 @@
-﻿global using RentShop_API.Models.Entities;
-global using RentShop_API.Models.Data;
-global using RentShop_API.Controllers;
-global using Microsoft.EntityFrameworkCore;
-global using System.Diagnostics.Metrics;
-global using RentShop_API.Interfaces;
-global using RentShop_API.Repository;
